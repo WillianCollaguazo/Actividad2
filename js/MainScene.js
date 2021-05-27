e7a43637f464feafb96ae1ae4aa39c090a064f36
Nico Chavez <nicopolis4991@hotmail.com>
2021-05-27 01:48:56 -0500

    creacion de botas para el doble salto

--- js/MainScene.js
+++ js/MainScene.js
@@ -11,8 +11,6 @@ class MainScene extends Phaser.Scene{
         this.load.image('sprites_doubleJump', 'res/doble jump.png');
         this.load.image('points', 'res/ScoreBoard.png');
 
-        //Phaser.Physics.Arcade.Sprite
-        // https://gammafp.com/tool/atlas-packer/
         this.load.atlas('sprites_jugador','res/player_anim/player_anim.png',
         'res/player_anim/player_anim_atlas.json');
 
@@ -37,40 +35,54 @@ class MainScene extends Phaser.Scene{
         var layerAssets = map.createLayer('Assets', tiles, 0, 0);
         var layerBanner = map.createLayer('Banner', tiles, 0, 0);
         var layerLadder = map.createLayer('Ladder', tiles, 0, 0);
+        var LayerDisk = map.createLayer('Disks', tiles, 0, 0);
         
 
         //enable collisions for every tile
-        layerGround.setCollisionByExclusion(-1,true);
-        layerLadder.setCollisionByExclusion(-1,true);
+        layerGround.setCollisionByExclusion(-1, true);
 
         //necesitamos un player
-        this.player = new Player(this,145,263);
-        this.bird = new Bird(this,895,318,340);      
-        this.jumpDouble = new DoubleJump(this, 465, 300);
+        this.player = new Player(this, 145, 263);
+        this.bird = new Bird(this, 895, 318,340);      
 
         this.physics.add.collider(this.player,layerGround);
         this.physics.add.collider(this.bird,layerGround);
-
+        
         this.physics.add.overlap(this.bird, this.player, this.deadPlayer,null,this);  
-        this.physics.add.overlap(this.jumpDouble, this.player, this.dobleJump, null, this);     
+           
 
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         this.cameras.main.startFollow(this.player);
 
-        this.objectsJSON = map.getObjectLayer('Items')['objects'];
+        //creacion de los discos colectables
+        var objectsJSON = map.getObjectLayer('Disks')['objects'];
         this.disks = [];
-        for(let i = 0; i < this.objectsJSON.length; i++)
+        for(let i = 0; i < objectsJSON.length; i++)
         {
             var obj = objectsJSON[i];
             if(obj.gid == 213) // gid de los discos
             {
-                this.disks.push(new Disk(this, obj.x, obj.y));
-                // var disks = new Disk(this,obj.x,obj.y);
-                // this.disks.push(disk);
-                // this.physics.add.overlap(disk, this.player, this.spriteHit,null,this);
+                this.disks.push(new Disk (this, obj.x, obj.y));
+                this.physics.add.overlap(this.disks, this.player, this.spriteHit,null,this);
             }
         }
+        this.physics.add.collider(this.disks, layerGround);
 
+        //creacion de los zapatos para el doble salto
+        var objBoot= map.getObjectLayer('DogleJump')['objects'];
+        this.boots = [];
+        for(let i = 0; i < objBoot.length; i++)
+        {
+             var objB = objBoot[i];
+             if(objB.gid == 225) // gid de los zapatos
+             {
+                 this.boots.push(new DoubleJump (this, objB.x, objB.y));
+                 this.physics.add.overlap(this.boots, this.player, this.dobleJump, null, this);  
+             }
+         }
+    
+        
+        //scoreboard
         var points = this.add.sprite(90,55,'points').setScrollFactor(0);
         this.score = 0;
         this.scoreText = this.add.text(30, 35, 'PUNTOS: '+this.score, { 
@@ -79,6 +91,7 @@ class MainScene extends Phaser.Scene{
             fontFamily: 'verdana, arial, sans-serif'
           });
     }
+
 
     //función para crear background infinito con efecto parallax
     createLoop(scene, count, texture, scrollFactor){
@@ -95,14 +108,13 @@ class MainScene extends Phaser.Scene{
     }
 
     deadPlayer (sprite1, sprite2) {
-        console.log("toco");
         this.player.RegresarInicio();
     }
 
     dobleJump(sprite1,sprite2)
     {
-        sprite1.setPosition(40,60);
-        this.player.doubleJump=true;
+        this.player.doubleJump = true
+        sprite1.setPosition(40,130).setScrollFactor(0); 
     }
 
     update (time, delta)
@@ -113,12 +125,12 @@ class MainScene extends Phaser.Scene{
 
         if(this.player.doubleJump)
         {
-            this.jumpDouble.x = this.cameras.main.scrollX + 40;
+            this.boots.x = this.cameras.main.scrollX + 40;
         }
     }
 
     showScore(){
-        this.score += 1;
+        this.score += 10;
         this.scoreText.setText('PUNTOS: ' + this.score);
     }
 
